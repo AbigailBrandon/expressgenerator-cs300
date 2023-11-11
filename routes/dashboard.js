1f51e0c6eea5d09f8549427d0e7f9cb8b7b9aa67
AbigailBrandon <abigailbrandonn@gmail.com>
2023-11-10 19:39:54 -0600

    Adjusting pages

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,11 @@
+
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET dashboard page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard');
+router.get('/', async function(req, res, next) {
+  let fakeData = await axios.get('https://jsonplaceholder.typicode.com/todos'); // example endpoint
+  res.render('dashboard', { fakeData: fakeData.data });
 });
 
 module.exports = router;
---
+++ routes/profiles.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  // Example fake user data
+  const fakeUserData = {
+    name: 'John Doe',
+    email: 'johndoe@example.com'
+  };
+  res.render('profile', { user: fakeUserData });
+});
+
+module.exports = router;
