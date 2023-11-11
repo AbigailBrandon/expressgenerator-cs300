18931a7a21d04e6031d4fbc8e03c7a08491f3ef8
AbigailBrandon <abigailbrandonn@gmail.com>
2023-11-10 20:15:30 -0600

    added bootstrap and fixing dashboard navigation

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,8 @@
-
 var express = require('express');
 var router = express.Router();
-var axios = require('axios');
 
-router.get('/', async function(req, res, next) {
-  let fakeData = await axios.get('https://jsonplaceholder.typicode.com/todos'); // example endpoint
-  res.render('dashboard', { fakeData: fakeData.data });
+router.get('/', function(req, res, next) {
+  res.render('dashboard'); 
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// In your profile route
+router.get('/', async function(req, res, next) {
+  // Assuming you have a User model for MongoDB
+  const userProfile = await User.findOne({ username: 'johndoe' }); // Fetch user profile
+  res.render('profile', { user: userProfile }); // Render with EJS
+});
+
 module.exports = router;
+
