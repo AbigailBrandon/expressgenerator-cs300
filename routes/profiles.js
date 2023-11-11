b84d13f90dff7ee149d8c146b2c488e34ca67ef9
AbigailBrandon <abigailbrandonn@gmail.com>
2023-11-10 19:56:38 -0600

    added .ejs and js content

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  // Example fake user data
-  const fakeUserData = {
-    name: 'John Doe',
-    email: 'johndoe@example.com'
-  };
-  res.render('profile', { user: fakeUserData });
-});
-
-module.exports = router;
+    // Example fake user data
+    const fakeUserData = {
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    };
+    res.render('users', { user: fakeUserData });
+  });
+  
+  module.exports = router;
